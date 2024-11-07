e6fc32dd0b699d0d0e5f529ab4ba8ecaacc5d739
seng0226 <118881380+seng0226@users.noreply.github.com>
2024-11-07 14:23:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
-const port = 8060;
+const port = 8099;
 // Use the whole root as static files to be able to serve the html file and
 // the build folder
 app.use(express.static(path.join(__dirname, '/')));
